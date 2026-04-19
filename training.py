a29b5be78d6ecff17249ff6ab1159e5f1a9172f9
Kishan Kumar <hskishanraj@gmail.com>
2026-04-19 15:25:35 +0530

    fix: pin memory issue

--- training.py
+++ training.py
@@ -101,8 +101,6 @@ class FashionTrainer(DefaultTrainer):
             cfg,
             mapper=mapper,
             num_workers=num_workers,
-            pin_memory=True,
-            persistent_workers=(num_workers > 0),
         )
 
     @classmethod
